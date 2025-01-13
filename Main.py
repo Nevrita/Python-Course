cab22e8d40c8707149352b8e047882595519221e
Heavenly <mehe.05nigar@gmail.com>
2025-01-14 00:41:29 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-print("Hi! I'm Meher")
+#Print a Sentence 
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("welcome to ", end= "*")

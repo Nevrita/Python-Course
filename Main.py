320362f6cc6e57712ab290ac693405d8171f3c74
Heavenly <mehe.05nigar@gmail.com>
2025-01-14 00:15:28 +0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+#Printing a greeting message
+print("Hello World!")
+
+#Printing my name
+print("I'm Meher")
+
+#Printing a fun fact about myself
+print("My ambition changes with time")
+
+#Printing a simple calclution
+print("The result of 3 + 5 is:", 3 + 5)

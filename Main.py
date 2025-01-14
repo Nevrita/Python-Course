1e6b65c1d11ccc686ffe0cb7c3e8f3fc37b4f1b4
Heavenly <mehe.05nigar@gmail.com>
2025-01-15 00:00:52 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,14 +1,13 @@
-#Print a Sentence
-print("Welcome to the world of programming!")
+# Creating different variables
+x = 5
+y = "Meher"
+print(x)
+print(y)
 
-#Print a Number
-print(4)
+codingal = "Codingal is a platfrom where kids love coding."
+print(codingal)
 
-#Next Line after print
-print("Hello Python \n")
+# User Input
+name = input("enter your name: ")
 
-#Pass multiple arguments for printing
-print("hello",5)
-
-#End argument of Print Statement
-print("welcome to", end= "*")
+print("\n Hello", name, "\n welcome to Codingal")

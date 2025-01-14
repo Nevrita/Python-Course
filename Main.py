87d069bf3817ed27c3876c2c293a93b4e973ca1b
Heavenly <mehe.05nigar@gmail.com>
2025-01-15 00:22:47 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
-import keyword
+#Printing my introduction
+print("Hello! I'm Meher.\n I'm from Bangladesh. \n Currently I'm in grade 9. \n Just like others I also have some hobbies. \n In my leisure, I love reading Si-Fi, listening to music, watching mystrious as well as horror movies and traveling. \n")
 
-# print all the Python keywords
-print("Python keywords are...\n")
-print(keyword.kwlist)
+#Printing my birthday
+Date = 17
+Month = "March"
+Year = 2012
+Day = "Wednesday"
+Time = "5:30 pm"
+
+print(Date)
+print(Month)
+print(Year)
+print(Day)
+print(Time)

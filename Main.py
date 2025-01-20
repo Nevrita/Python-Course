3d4cd54fe5adea978d87dcf62e61c5c6659439db
Heavenly <mehe.05nigar@gmail.com>
2025-01-21 00:08:28 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,15 +1,40 @@
-#Printing my introduction
-print("Hello! I'm Meher.\n I'm from Bangladesh. \n Currently I'm in grade 9. \n Just like others I also have some hobbies. \n In my leisure, I love reading Si-Fi, listening to music, watching mystrious as well as horror movies and traveling. \n")
+# Let's check the datatypes of different values
+a = 17
+print("type of a: ", type(a))
 
-#Printing my birthday
-Date = 17
-Month = "March"
-Year = 2012
-Day = "Wednesday"
-Time = "5:30 pm"
+b = 3.5
+print("type of b: ", type(b))
 
-print(Date)
-print(Month)
-print(Year)
-print(Day)
-print(Time)
+c = "I love coding."
+print("type of c: ", type(c))
+
+d = True
+print("type of d: ", type(d))
+
+# Assigning Different Variables
+name = "Meher"
+age = 12
+is_student = True
+weight = 30.6
+
+# Printing Different Variables and their Data Type
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+print("Is Student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+# Type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))

6410988be20aafb64ecc0536dfd3be0d90bef393
Heavenly <mehe.05nigar@gmail.com>
2025-01-21 00:56:52 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,27 +1,8 @@
-# Assigning Different Variables
-name = "Meher"
-age = 12
-is_student = True
-weight = 30.6
+# Getting the input (simulating Ram's input)  
+input_word = input("Enter the word in lowercase (e.g., 'congratulations'): ")  
 
-# Printing Different Variables and their Data Type
-print("Name :", name)
-print("Data Type of Name is", type(name))
+# Convert the input word to uppercase  
+uppercase_word = input_word.upper()  
 
-print("Age :", age)
-print("Data Type of Age is", type(age))
-
-print("Is Student :", is_student)
-print("Data Type of is_student is", type(is_student))
-
-print("Weight :", weight)
-print("Data Type of weight is", type(weight))
-
-# Type casting to convert the datatype of variables
-print("\n After Type Casting....")  # \n means next line
-age = str(age)
-print(age)
-print("Data Type of age is", type(age))
-weight = int(weight)
-print(weight)
-print("Data Type of Weight is", type(weight))
+# Display the result  
+print("The word in upper case is:", uppercase_word)

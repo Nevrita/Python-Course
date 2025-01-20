0614aee4e9a58d04945ead0559313ef22ad2ea01
Heavenly <mehe.05nigar@gmail.com>
2025-01-21 00:55:06 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Getting the input (simulating Ram's input)  
+input_word = input("Enter the word in lowercase (e.g., 'congratulations'): ")  
+
+# Convert the input word to uppercase  
+uppercase_word = input_word.upper()  
+
+# Display the result  
+print("The word in upper case is:", uppercase_word)

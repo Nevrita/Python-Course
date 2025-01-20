a74fd22070f29db0089efaeaa1f99ca8afa6e8cb
Heavenly <mehe.05nigar@gmail.com>
2025-01-21 00:20:28 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,40 +1,10 @@
-# Let's check the datatypes of different values
-a = 17
-print("type of a: ", type(a))
+#input a word
+text = str(input("Enter a string: "))
 
-b = 3.5
-print("type of b: ", type(b))
+#Reverse String
+# using step value as -1 to iterate in reverse
+revText = text[::-1]
+text = revText
 
-c = "I love coding."
-print("type of c: ", type(c))
-
-d = True
-print("type of d: ", type(d))
-
-# Assigning Different Variables
-name = "Meher"
-age = 12
-is_student = True
-weight = 30.6
-
-# Printing Different Variables and their Data Type
-print("Name :", name)
-print("Data Type of Name is", type(name))
-
-print("Age :", age)
-print("Data Type of Age is", type(age))
-
-print("Is Student :", is_student)
-print("Data Type of is_student is", type(is_student))
-
-print("Weight :", weight)
-print("Data Type of weight is", type(weight))
-
-# Type casting to convert the datatype of variables
-print("\n After Type Casting....")
-age = str(age)
-print(age)
-print("Data Type of age is", type(age))
-weight = int(weight)
-print(weight)
-print("Data Type of Weight is", type(weight))
+print("Reverse of Given String is:")
+print(text)

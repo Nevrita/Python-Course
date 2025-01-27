af22d92befbc85f06faea5305b6d8541f1610fda
Heavenly <mehe.05nigar@gmail.com>
2025-01-28 00:06:29 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,21 +1,9 @@
-import math  
+costprice =int(input("enter the cp: "))
+sellingprice =int(input("enter the sp: "))
 
-def calculate_square_root():  
-    try:  
-        number = float(input("Enter a number to find its square root: "))  
-        if number < 0:  
-            print("Sorry, square root of a negative number is not defined.")  
-        else:  
-            square_root = math.sqrt(number)  
-            # Check if the input number is an integer  
-            if number.is_integer():  
-                number = int(number)  
-            # Print the square root as an integer if it is a whole number  
-            if square_root.is_integer():  
-                square_root = int(square_root)  
-            print(f"The square root of {number} is {square_root}")  
-    except ValueError:  
-        print("Invalid input! Please enter a valid number.")  
-
-if __name__ == "__main__":  
-    calculate_square_root()
+if(sellingprice>costprice):
+    print("profit")
+    pt=sellingprice-costprice
+    print(pt)
+else :
+    print("No profit")

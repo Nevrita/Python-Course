8761c5b18f44b880b6489dee327f2add33d7ff9d
Heavenly <mehe.05nigar@gmail.com>
2025-01-28 00:26:41 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,13 @@
-n=int(input("Enter the number:"))
-if( n%2==0 ):
- print("The number is even")
+# quiz.py
+
+answer = input("When was the first known use of the word 'quiz'? ")
+if answer == "1781":
+    print("Correct!")
 else:
- print("The number is odd")
+    print(f"The answer is '1781', not {answer!r}")
+
+answer = input("Which built-in function can get information from the user? ")
+if answer == "input":
+    print("Correct!")
+else:
+    print(f"The answer is 'input', not {answer!r}")

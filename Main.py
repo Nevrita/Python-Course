5ff3472609888032018ce3ae2212fcbf9e57c6c3
Heavenly <mehe.05nigar@gmail.com>
2025-01-28 23:44:04 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,14 +1,7 @@
-def check_clothing(temperature):  
-    if temperature > 20:  
-        return "It's warm enough for light clothes!"  
-    elif 15 < temperature <= 20:  
-        return "You might want a light jacket, but light clothes should be fine."  
-    elif 10 < temperature <= 15:  
-        return "It's getting a bit chilly; a sweater might be a good idea."  
-    else:  
-        return "It's too cold for light clothes. Better wear a jacket!"  
-
-# Example usage  
-current_temperature = float(input("Enter the current temperature in °C: "))  
-suggestion = check_clothing(current_temperature)  
-print(suggestion)
+a = 23
+b = 24
+c = 22
+if a > b or a < c:
+    print("I'm using or operator")
+elif a < b and a > c:
+    print("I'm using and operator")

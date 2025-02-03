2bc7e321aead809595efa9b628202406cfb68dab
Heavenly <mehe.05nigar@gmail.com>
2025-02-03 23:56:08 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,8 +1,14 @@
-# Get the character from the user  
-character = input("Enter a character: ")  
+#Take input for the student that he can attend the exam or not
+medical_cause=input("Did you have a medical cause Yes or No: ")
 
-# Check if the character is an alphabet  
-if character.isalpha() and len(character) == 1:  
-    print(f"'{character}' is an alphabet.")  
-else:  
-    print(f"'{character}' is not an alphabet.")
+#Take input of the attendance
+attendance = int(input("Enter the attendance of the student: "))
+
+#Checking the user input predicting output accordingly
+if medical_cause == 'Yes': #Checking the condition 1
+    print ("You are allowed")
+else:
+    if attendance>=75: #Checking the condition 2
+        print ("Allowed")
+    else:
+        print("Not allowed")

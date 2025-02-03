b6282f50e6ecb102eced857cb3f58a95d1e1b6c0
Heavenly <mehe.05nigar@gmail.com>
2025-02-04 00:22:30 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,14 +1,27 @@
-#Take input for the student that he can attend the exam or not
-medical_cause=input("Did you have a medical cause Yes or No: ")
+# Take input of number of units consumed from the user
+units = int(input(" Please enter Number of Units you Consumed : "))
 
-#Take input of the attendance
-attendance = int(input("Enter the attendance of the student: "))
+# Check conditions of units consumed
+# Then calculate amount and surcharge accordingly
+# surcharge is the tax value
 
-#Checking the user input predicting output accordingly
-if medical_cause == 'Yes': #Checking the condition 1
-    print ("You are allowed")
+# Check for units less than 50
+if(units < 50):
+    amount = units * 2.60
+    surcharge = 25
+
+# Check for units less than or equal to 200
+elif(units <= 200):
+    amount= 130 + 162.50 + ((units - 100) * 5.26)
+    surcharge = 45
+
+# Check for all the cases other than mentioned
+# When units consumed are more than 200
 else:
-    if attendance>=75: #Checking the condition 2
-        print ("Allowed")
-    else:
-        print("Not allowed")
+    amount = 130 + 162.50 +526 + ((units - 200) * 8.45)
+    surcharge = 75
+
+# Calculate and display the total electricity bill
+# Total amount = amount + surcharge
+total = amount + surcharge
+print("\n Electricity Bill = %.2f"  %total)

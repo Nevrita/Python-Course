b574a18da86035063ce30d5625a4e5391985aa94
Heavenly <mehe.05nigar@gmail.com>
2025-02-05 00:02:03 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,19 +1,6 @@
-# Program to check if a student can enroll based on age  
-
-def check_enrollment_age():  
-    print("Welcome! Let's check if you can enroll in grade 10.")  
-    try:  
-        age = int(input("Please enter your age: "))  
-
-        if 10 <= age <= 20:  
-            print("You are eligible to enroll in Raj's class! 🎉")  
-        elif age > 20:  
-            print("Sorry, you are too old to enroll in Raj's class. 😔")  
-        else:  
-            print("Sorry, you are too young to enroll in Raj's class. 🌼")  
-
-    except ValueError:  
-        print("Oops! Please enter a valid number for your age. 😅")  
-
-# Call the function to execute  
-check_enrollment_age()
+age=int(input("Enter your age:"))
+if age>=10:
+    if age<=20:
+        print ("Your age is : ", age," \n You are allowed to enter the class")
+    else:
+        print ("You age is:",age , " \n You are not eligible")

d05219ea49f536f6c1415f092d9d712e58ba03e2
Heavenly <mehe.05nigar@gmail.com>
2025-03-04 00:11:46 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,15 +1,8 @@
-for x in range(10):     # Iterate for loop
-    if x % 20 == 0:     # Condition 1
-        print("twist")
-
-    elif x % 15 == 0:   # Condition 2
-        pass            # Pass statement
-
-    elif x % 5 == 0:    # Condition 3
-        print("fizz")   # Display result
-
-    elif x % 3 == 0:    # Condition 4
-        print("buzz")   # Display result
-
-    else:               # Condition 5
-        print(x)        # Display result
+var = 10            # Initialise
+while var > 0:      # Iterate loop 
+    var = var - 1
+    if var == 5:    # Condition 1
+        continue    # Continue statement
+        # Display result
+    print ('\n Current variable value :', var)
+    print ("\n Good Bye!!!")

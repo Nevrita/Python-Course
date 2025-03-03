56296585f326dc1331e7d63da753149d1d2d5318
Heavenly <mehe.05nigar@gmail.com>
2025-03-04 03:10:08 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,8 +1,17 @@
-var = 10            # Initialise
-while var > 0:      # Iterate loop 
-    var = var - 1
-    if var == 5:    # Condition 1
-        continue    # Continue statement
-        # Display result
-    print ('\n Current variable value :', var)
-    print ("\n Good Bye!!!")
+def calculate_change(total_bill, amount_paid):  
+    if amount_paid < total_bill:  
+        return - 1
+    change = amount_paid - total_bill  
+    if change == 0:  
+        return 0
+    return change 
+total_bill = 2.50  
+amount_paid = 4  
+change = calculate_change(total_bill, amount_paid)  
+
+if change < 0:  
+    print("The payment is insufficient.")  
+elif change == 0:  
+    print("There will be no changes to return.")  
+else:
+    print ("The shopkeeper must return Vishal a total of $" + str(change))

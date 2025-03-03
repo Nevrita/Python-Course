b6655f8f044c4318ec45943db76417a0d9bf9ff6
Heavenly <mehe.05nigar@gmail.com>
2025-03-03 23:50:44 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
-def shutdown():  
-    user_input = input("Do you want to shut down? (Yes/No): ")  
-
-    if user_input == "Yes":  
-        print("Shutting down...")  
-    elif user_input == "No":  
-        print("Hold on, we're not shutting down just yet!")  
-    else:  
-        print("Sorry.") 
-
-shutdown()
+# Take user input
+a = input ("Enter a word: ")
+#program to check break keyword
+for i in a:         # Iterate for loop
+    if (i == 'A'):  # Condition 1
+    # Display result
+        print ("A is found")
+        break
+    else:
+    # Display result   
+        print ("A not found")

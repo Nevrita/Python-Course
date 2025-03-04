201caf9b1e233771a84f55ebfda8c0f47b1cbe97
Heavenly <mehe.05nigar@gmail.com>
2025-03-05 00:22:42 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,17 +1,7 @@
-try:
-    num1 = int(input("Enter a number: "))
-    num2 = int(input("Enter a number: "))
-    result = num1/num2
-    print("Result is : ", result)
+try :
+    num = int(input("Enter your number : "))
+    print(num)
+except ValueError as ex:
+    print("Exception: ",ex)
 
-except ZeroDivisionError:
-    print("Division by zero is not not allowed")
-except ValueError:
-    print("Please enter numerial value")
-except NameError as ex:
-    print("The exception is ",ex)
-
-except:
-    print("Some errror occurred")
-finally:
-    print("I will execute no matter what happens")
+print("I am outside the try block")

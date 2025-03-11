5ff9095b16f255a9a3a38a5f717a1322f23a5b70
Heavenly <mehe.05nigar@gmail.com>
2025-03-12 00:30:25 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
-def hotel_cost(nights):
-    return 140*nights
+def information():
+    print("learning python")
+    print("using Visual Studio Code platform")
 
-def plane_ride_cost(city):
-    if "Charlotte" == city:
-        return 183
-    elif "Tampa" == city:
-        return 220
-    elif "Pittsburgh" == city:
-        return 222
-    elif "Los Angeles" == city:
-        return 475
+information()
+information()
+information()
+information()
+information()
+
+def add(num1,num2):
+  return num1+num2
+
+print(add(3,5))

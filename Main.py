0fd4bbe4b7d1e2763bdc56109ffdfeab5e08517b
Heavenly <mehe.05nigar@gmail.com>
2025-03-25 01:12:00 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,18 +1,12 @@
-# Initialize dictionary
-test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+test_dict = {'Codingal': 3, 'is': 2, 'best': 2, 'for': 2, 'Coding': 1}  
 
-# printing original dictionary
-print("The original dictionary : " + str(test_dict))
+print("The original dictionary : " + str(test_dict))  
 
-# Initialize value 
-K = 2
+K = int(input("Enter the value to check the frequency: "))  
 
-# Using loop
-# Selective key values in dictionary
-res = 0
-for key in test_dict:
-    if test_dict[key] == K:
-        res = res + 1
+result = 0  
+for key in test_dict:  
+    if test_dict[key] == K:  
+        result += 1 
 
-# printing result 
-print("Frequency of K is :" + str(res))
+print("Frequency of", K, "is :", result)  

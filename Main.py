38084feecd5480c0a024f6ede6fdcba1aac1eb8f
Heavenly <mehe.05nigar@gmail.com>
2025-03-25 00:16:24 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,29 +1,11 @@
-student_data = {'id1': 
-   {'name': ['Sara'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
- 'id2': 
-  {'name': ['David'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
- 'id3': 
-    {'name': ['Sara'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
- 'id4': 
-   {'name': ['Surya'], 
-    'class': ['V'], 
-    'subject_integration': ['english, math, science']
-   },
-}
+country_code = {'India' : '0091',
+                'Australia' : '0025',
+                 'Nepal' : '00977'}
 
-result = {}
+# search dictionary for country code of India
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
 
-for key,value in student_data.items():
-    if value not in result.values():
-        result[key] = value
-
-print(result)
+# search dictionary for country code of Japan
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))

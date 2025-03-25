4eb731ff00cb12cc041f4e681e2f845b90ba33de
Heavenly <mehe.05nigar@gmail.com>
2025-03-26 00:03:37 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,20 +1,8 @@
-# Different types of sets in Python
-# set of integers
-my_set = {1, 2, 3}
-print(my_set)
-
-# set of mixed datatypes
-my_set = {1, 2, 3, 4, 3, 2}
-print(my_set)
-
-# we can make set from a list
-my_set = set([1, 2, 3, 2])
-print(my_set, "\n")
-
-#remove a number from a set
-num_set = set([0, 1, 3, 4, 5])
-print("Original set: ")
-print(num_set)
-num_set.pop()
-print("After removing the first element from the said set: ")
-print(num_set, "\n")
+setx = {"Black", "Red"}
+sety = {"Red", "White"}
+print("Original set elements: ")
+print(setx)
+print(sety)
+print("\n Intersection of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)

f6e21236afb2752f56b5f61a2cf75b2a4e063fd3
Heavenly <mehe.05nigar@gmail.com>
2025-04-08 01:16:26 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,14 +1,28 @@
-# Python program to demonstrate
-# exit()
+def generate_odd_numbers(num):  
+    return [x for x in range(num + 1) if x % 2 != 0]  
 
-for i in range(10):
-	
-	# If the value of i becomes
-	# 5 then the program is forced
-	# to exit
-    if i == 5:
+def get_capitalized_fruits():  
+    fruits_input = input("Enter a list of fruits separated by commas: ")  
 
-        # prints the exit message
-        print(exit)
-        exit()
-    print(i)
+    fruits = []  
+    current_fruit = ""  
+    for charr in fruits_input:  
+        if charr == ',':  
+            if current_fruit: 
+                fruits.append(current_fruit)  
+            current_fruit = ""  
+        else:  
+            current_fruit += charr  
+            
+    if current_fruit:
+        fruits.append(current_fruit)  
+
+    return [fruit.capitalize() for fruit in fruits]  
+
+num = int(input("Enter a maximum number to generate odd numbers: "))  
+odd_numbers = generate_odd_numbers(num)  
+
+capitalized_fruits = get_capitalized_fruits()  
+  
+print("List of odd numbers:", odd_numbers)  
+print("List of capitalized fruits:", capitalized_fruits)  

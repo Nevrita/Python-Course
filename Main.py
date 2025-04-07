d44ed8e4919ccda3061dc222fc5eb652f544ab5e
Heavenly <mehe.05nigar@gmail.com>
2025-04-07 23:49:38 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
-set1_A = {'blue', 'white'}  
-set2_A = {'white', 'black'}  
+# Add two lists using map and lambda
 
-symmetric_difference_A = set1_A.symmetric_difference(set2_A)  
-print("Symmetric Difference A: ", symmetric_difference_A)  
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print("Addition of two lists")
+print(list(result))
 
-set1_B = {1, 2, 3, 4, 5}  
-set2_B = {1, 5, 6, 7, 8, 9}  
 
-symmetric_difference_B = set1_B.symmetric_difference(set2_B)  
-print("Symmetric Difference B: ", symmetric_difference_B)  
+#using map
+nums = [1, 2, 3, 4, 5]
+def sq(n):
+    return n*n
+square = list(map(sq, nums))
+print("Square of numbers in list")
+print(square)

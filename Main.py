5b30d4aa55f37ac22f248341697c563b5622cee2
Heavenly <mehe.05nigar@gmail.com>
2025-05-14 01:37:27 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,16 +1,11 @@
-class Vehicle:
-    def __init__(self, capacity, fare_per_seat):
-        self.capacity = capacity
-        self.fare_per_seat = fare_per_seat
+class ReverseString:
+    def __init__(self, s):
+        self.s = s
 
-class Bus(Vehicle):
-    def __init__(self, capacity, fare_per_seat):
-        super().__init__(capacity, fare_per_seat)
+    def get_reversed(self):
+        return self.s[::-1]
 
-    def calculate_fare(self, num_passengers):
-        total_fare = self.fare_per_seat * num_passengers * 1.10
-        return total_fare
+word = input("Enter a word: ")
 
-bus = Bus(50, 100)
-fare = bus.calculate_fare(50)
-print("Total fare: INR", fare)
+reverser = ReverseString(word)
+print("Reversed word:", reverser.get_reversed())

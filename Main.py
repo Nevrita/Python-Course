cccaa280416a651155a2b27f5a472a9eec072cc9
Heavenly <mehe.05nigar@gmail.com>
2025-05-21 00:36:09 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,28 +1,34 @@
-class flashcard:
-	def __init__(self, word, meaning):
-		self.word = word
-		self.meaning = meaning
-	def __str__(self):
-		
-		#we will return a string
-		return self.word+' ( '+self.meaning+' )'
-		
-flash = []
-print("welcome to flashcard application")
+import random
 
-#the following loop will be repeated until
-#user stops to add the flashcards
-while(True):
-	word = input("enter the name you want to add to flashcard : ")
-	meaning = input("enter the meaning of the word : ")
-	
-	flash.append(flashcard(word, meaning))
-	option = int(input("enter 0 , if you want to add another flashcard otherwise enter 1 : "))
-	
-	if(option):
-		break
+class FruitQuiz:
+
+	# Create a constructor
+	def __init__(self):
 		
-# printing all the flashcards
-print("\nYour flashcards")
-for i in flash:
-	print(">", i)
+		# Create a dictionary of fruits as keys and color as value
+		self.fruits={'apple':'red',
+					'orange':'orange',
+					'watermelon':'green',
+					'banana':'yellow'}
+
+	# Function for the quiz, here a fruit will be chosen randomly	
+	def quiz(self):
+		while (True):
+			
+			fruit, color = random.choice(list(self.fruits.items()))
+			
+			print("What is the color of {}".format(fruit))
+			user_answer = input()
+			
+			if(user_answer.lower() == color):
+				print("Correct answer")
+			else:
+				print("Wrong answer")
+				
+			option = int(input("enter 0 , if you want to play again otherwise enter 1: "))
+			if (option):
+				break
+
+print("welcome to fruit quiz ")
+fq = FruitQuiz()
+fq.quiz()

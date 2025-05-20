4336af5ad7a797294fb421fbe6bb7cf8974db14b
Heavenly <mehe.05nigar@gmail.com>
2025-05-21 01:11:40 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,34 +1,21 @@
-import random
+class IntegerToRoman:
+    def __init__(self, num):
+        self.num = num
 
-class FruitQuiz:
+    def get_roman(self):
+        values = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+            (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+        result = ''
+        num = self.num
+        for value, symbol in values:
+            while num >= value:
+                result += symbol
+                num -= value
+        return result
 
-	# Create a constructor
-	def __init__(self):
-		
-		# Create a dictionary of fruits as keys and color as value
-		self.fruits={'apple':'red',
-					'orange':'orange',
-					'watermelon':'green',
-					'banana':'yellow'}
-
-	# Function for the quiz, here a fruit will be chosen randomly	
-	def quiz(self):
-		while (True):
-			
-			fruit, color = random.choice(list(self.fruits.items()))
-			
-			print("What is the color of {}".format(fruit))
-			user_answer = input()
-			
-			if(user_answer.lower() == color):
-				print("Correct answer")
-			else:
-				print("Wrong answer")
-				
-			option = int(input("enter 0 , if you want to play again otherwise enter 1: "))
-			if (option):
-				break
-
-print("welcome to fruit quiz ")
-fq = FruitQuiz()
-fq.quiz()
+number = int(input("Enter an integer: "))
+roman_converter = IntegerToRoman(number)
+print(roman_converter.get_roman())
